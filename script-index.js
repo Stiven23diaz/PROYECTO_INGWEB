7b7db44085a14c1e997d4d93c037fbdc35e1a41f
Stiven23diaz <josestivendiaz23@gmail.com>
2026-06-27 16:39:44 -0500

    ultimos cambios

--- script-index.js
+++ script-index.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const nombre = document.getElementById("nombre");
     const correo = document.getElementById("correo");
     const mensaje = document.getElementById("mensaje");
+    const enviar = document.getElementById("enviar");
 
     // Nombre
     nombre.addEventListener("invalid", function () {
@@ -50,7 +51,6 @@ document.addEventListener("DOMContentLoaded", () => {
     mensaje.addEventListener("input", function () {
         this.setCustomValidity("");
     });
-    
      window.addEventListener("scroll", function () {
 
         if (window.scrollY > 700) {
@@ -73,4 +73,33 @@ document.addEventListener("DOMContentLoaded", () => {
 
     });
 
-});
+    enviar.addEventListener("click", function (event) {
+        if (nombre.checkValidity() && correo.checkValidity() && mensaje.checkValidity()) {
+            alert("MENSAJE ENVIADO");
+        } else {
+            alert("COMPLETAR TODOS LOS CAMPOS CORRECTAMENTE");
+        }})
+
+        // la seccion del api del clima no tocaaar
+
+        const btnClima = document.getElementById("btnClima");
+        btnClima.addEventListener("click", obtenerClima);
+
+        function obtenerClima() {
+
+            fetch("https://api.open-meteo.com/v1/forecast?latitude=4.6097&longitude=-74.0817&current=temperature_2m,wind_speed_10m")
+
+        .then(respuesta => respuesta.json())
+        .then(datos => {
+            document.getElementById("temperatura").textContent =
+                "Temperatura: " + datos.current.temperature_2m + " °C";
+            document.getElementById("viento").textContent =
+                "Velocidad promerdio del viento " + datos.current.wind_speed_10m + " km/h";
+        })
+        .catch(error => {
+            console.log(error);
+            alert("No fue posible obtener los datos del clima.");
+
+        });
+}
+    });
